7a7aca896f6e448c5cbd182720df8d43e7f8af16
gwonhyeong.tak <gwonhyeong.tak@gmail.com>
2022-05-15 23:14:52 +0900

    [title] add splash & no actionbar style

---
+++ tBaseApp/app/src/main/java/com/ghtak/baseapp/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.ghtak.baseapp
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import com.ghtak.baseapp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.apply {
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
+        }
+
+        Handler().postDelayed(
+            {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            },1000
+        )
+    }
+}
